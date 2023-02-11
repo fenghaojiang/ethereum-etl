8918feadf58df2b140c6fab3919f93f5e2d777c7
jiang <fenghaojiang97@m.scnu.edu.cn>
2023-02-11 11:32:49 +0800

    initial commit

---
+++ gen_script.sh
@@ -0,0 +1,4 @@
+
+abigen --abi=./abis/erc/erc20/erc20.abi --pkg=erc20 --out=./abis/erc/erc20/erc20.go
+abigen --abi=./abis/erc/erc721/erc721.abi --pkg=erc721 --out=./abis/erc/erc721/erc721.go
+abigen --abi=./abis/erc/erc1155/erc1155.abi --pkg=erc1155 --out=./abis/erc/erc1155/erc1155.go
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fenghaojiang/ethereum-etl
+
+go 1.19
