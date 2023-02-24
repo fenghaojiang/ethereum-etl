8cbcefe606aa63df619175e653b09e1047f09f29
J1ang <fenghaojiang97@m.scnu.edu.cn>
2023-02-24 23:58:38 +0800

    add fixer method

--- plugin/fixer/fixer.go
+++ plugin/fixer/fixer.go
@@ -1,6 +1,19 @@
 package fixer
 
+var _ IFixer = (*Fixer)(nil)
+
 type IFixer interface {
 	CheckReorg(blockNumber uint64, currentBlockHash string) (previousBlockHash string, isReorg bool)
 	fixReorg(ch chan<- struct{}) error
+}
+
+type Fixer struct {
+}
+
+func (f *Fixer) CheckReorg(blockNumber uint64, currentBlockHash string) (previousBlockHash string, isReorg bool) {
+	return "", false
+}
+
+func (f *Fixer) fixReorg(ch chan<- struct{}) error {
+	return nil
 }
