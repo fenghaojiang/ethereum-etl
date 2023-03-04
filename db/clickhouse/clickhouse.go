654d59146e2e2ffd89365a8bcef3aa33f9838201
jiang <fenghaojiang97@m.scnu.edu.cn>
2023-03-04 23:58:20 +0800

    feat: add clickhouse db driver

---
+++ db/clickhouse/clickhouse.go
@@ -0,0 +1,31 @@
+package clickhouse
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/fenghaojiang/ethereum-etl/db"
+)
+
+// TODO
+type Driver struct {
+	db *sql.DB
+}
+
+func newDriver(db.DriverConfig) db.IDriver {
+	return &Driver{}
+}
+
+func (d *Driver) Ping(ctx context.Context) error {
+	return nil
+}
+func (d *Driver) Close(ctx context.Context) error {
+	return nil
+}
+func (d *Driver) Open(ctx context.Context, dbType db.DBType, connectionConfig db.ConnectionConfig) (db.IDriver, error) {
+	return nil, nil
+}
+
+func (d *Driver) GetConnection(ctx context.Context, database string) (*sql.DB, error) {
+	return nil, nil
+}
